5e1a3e32a0e34701d542c308334ca2ce67741553
Ирина Притчина <90133646+IrinBtr4ik@users.noreply.github.com>
2026-06-22 11:16:55 +0700

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,15 +2,15 @@ DB_CONFIG = {
     'server': 'localhost',           # Имя сервера (или IP)
     'port': 1433,                    # Порт (по умолчанию 1433)
     'user': 'LENOVO-1041623\SQLEXPRESS',                    # Логин SQL Server
-    'password': '', # Пароль
-    'database': 'HotelDB'         # Имя базы данных
+    'password': 'BotPassword123!', # Пароль
+    'database': 'HotelBotDB'         # Имя базы данных
 }
 
 # Альтернативная строка подключения для pyodbc
 CONNECTION_STRING = (
     "DRIVER={ODBC Driver 17 for SQL Server};"
     "SERVER=localhost;"
-    "DATABASE=HotelDB;"
+    "DATABASE=HotelBotDB;"
     "UID=LENOVO-1041623\SQLEXPRESS;"
-    "PWD="
+    "PWD=BotPassword123!"
 )
